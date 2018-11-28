00de82dd78cc777abfbd83febe148f260f1a704e
xDangerousDinox <ahoyt2014@gmail.com>
2018-11-27 16:55:42 -0800

    Work on PID

--- Programming-Level-2/src/main/java/frc/robot/subsystems/Arm.java
+++ Programming-Level-2/src/main/java/frc/robot/subsystems/Arm.java
@@ -7,13 +7,12 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.command.*;
-import frc.robot.*;
-import frc.robot.commands.*;
-import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import frc.robot.RobotMap;
 
 public class Arm extends PIDSubsystem {
   private WPI_TalonSRX arm;
@@ -28,12 +27,15 @@ public class Arm extends PIDSubsystem {
   public static final double startingAngle = -50;
 
   public Arm() {
-    super("Arm", 1.0, 0.0, 0.0);
+    super("ArmPID", 1.0, 1.0, 1.0);
     this.setAbsoluteTolerance(0.2);
     this.getPIDController().setContinuous(false);
 
     this.arm = new WPI_TalonSRX(RobotMap.armMotor);
-    
+    this.arm.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+    this.arm.setSensorPhase(false);
+    this.arm.setSelectedSensorPosition(0, 0, 0);
+
     this.flag = new Servo(0);
     //servoStart = servoAngle();
   }
@@ -49,7 +51,7 @@ public class Arm extends PIDSubsystem {
 
   @Override
   protected void usePIDOutput(double output) {
-    this.arm.pidWrite(direction * output);
+    this.arm.pidWrite(output);
   }
 
   public void startMotor(int direction) {
